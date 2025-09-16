dfeb0db752bba20ebafb713f0983d1e134625cee
chikichikibangbang <2239389326@qq.com>
2025-09-16 09:13:14 +0800

    V0.1 - Beta initial release

--- addons/MHW_Model_Editor/__init__.py
+++ addons/MHW_Model_Editor/__init__.py
@@ -17,7 +17,7 @@ bl_info = {
     "name": "MHW Model Editor",
     "author": "诸葛不太亮, NSACloud",
     "blender": (2, 93, 0),
-    "version": (0, 2),
+    "version": (0, 1),
     "description": "Import, edit and export MHW Model (mod3, mrl3, ctc, ccl) files.",
     "warning": "",
     "wiki_url": "",
